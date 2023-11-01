78e6e6b53425b4c171d3a5956bdf167a32212292
John <johnboone215@hotmail.com>
2023-10-31 22:45:10 -0500

    Update update-asa-server.ps1

--- update-asa-server.ps1
+++ update-asa-server.ps1
@@ -1,8 +1,5 @@
-# Prompt for the SteamCMD installation directory
-$steamCmdPath = Read-Host "Enter the path where SteamCMD is installed"
-
 # Prompt for the ARK server installation directory
-$installPath = Read-Host "Enter the path where your ARK Survival Ascended server is installed"
+$installPath = Read-Host "Enter the path where your ARK: Survival Ascended server is installed"
 
 # Check if the ARK server path exists
 if (-not (Test-Path $installPath)) {
@@ -10,11 +7,29 @@ if (-not (Test-Path $installPath)) {
     exit
 }
 
-# Install or update ARK server using the correct AppID (2430930)
-$arkServerCmd = Join-Path $steamCmdPath "steamcmd.exe"
+# Update ARK server using the correct AppID (2430930)
+$arkServerCmd = Join-Path $installPath "ShooterGame\Binaries\Win64\start.bat"
+
+# Check if the start.bat file exists
+if (-not (Test-Path $arkServerCmd)) {
+    Write-Host "start.bat file does not exist. Please create it first before running this update script."
+    exit
+}
+
 $forceInstallDir = "+force_install_dir $installPath"
 $appUpdate = "+app_update 2430930 validate"
 
-& $arkServerCmd $forceInstallDir "+login anonymous" $appUpdate "+quit"
+# Run the update command using SteamCMD
+$steamCmdPath = Join-Path $installPath "steamcmd.exe"
+& $steamCmdPath $forceInstallDir "+login anonymous" $appUpdate "+quit"
 
-Write-Host "ARK Survival Ascended server has been updated in $installPath."
+# Start the ARK: Survival Ascended server
+Start-Process -FilePath $arkServerCmd -NoNewWindow
+
+# Check if the ARK server process is running
+$arkProcess = Get-Process -name "arkascendedserver" -ErrorAction SilentlyContinue
+if ($null -ne $arkProcess) {
+    Write-Host "ARK: Survival Ascended server has been updated and is running."
+} else {
+    Write-Host "ARK: Survival Ascended server update completed, but the server process is not running. Please check for errors."
+}
